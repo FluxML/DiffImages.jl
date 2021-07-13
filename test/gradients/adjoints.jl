6937f982febbdd6dbe963cca562b50f66c45589b
SomTambe <tambesom@gmail.com>
2021-07-13 12:44:59 +0530

    Add tests for testing the adjoint using FiniteDifferences

--- test/gradients/adjoints.jl
+++ test/gradients/adjoints.jl
@@ -1,11 +1,17 @@
 # Tests for different adjoints written here.
 L(x) = sum(x)
+
 function warp_me2(img::AbstractArray{T}, tform) where T
    img = ImageTransformations.box_extrapolation(img, Flat())
    inds = DiffImages.custom_autorange(img, inv(tform))
    inds = map(x->SVector{length(x),Float64}(x.I),CartesianIndices(inds))
    img = map(x -> ImageTransformations._getindex(img, tform(x)), inds)
    return
+end
+
+function index_warp(img::AbstractArray{T}, tform, inds::Tuple) where T
+   img = ImageTransformations.box_extrapolation(img, Flat())
+   return ImageTransformations._getindex(img, tform(SVector(inds)))
 end
 
 @testset "Testing ∂g/∂(i,j)" begin
@@ -44,17 +50,35 @@ end
    h = homography(rand(SMatrix{3,3,Float64,9}))
    img_plain = rand(Float64, 10, 10)
    img_src = rand(RGB, 10, 10)
+
+   @testset "Adjoint testing" begin
+      f(tform) = begin
+         tform = homography(SMatrix{3,3,Float64,9}(tform))
+         return sum(index_warp(img_plain, tform, (3,4)))
+      end
+
+      ∇₁ = Zygote.gradient(img_plain, h, (3,4)) do img, tform, inds
+               c = sum(index_warp(img, tform, inds))
+               c
+         end[2].H
+
+      ∇₂ = grad(central_fdm(5,1), f, Array(h.H))[1]
+      
+      @test ∇₁ ≈ ∇₂
+   end
+
    @testset "FloatTypes testing" begin
       outp = Zygote.gradient(img_plain, h) do img, tform
-                   c = L(warp_me2(img, tform))
+                   c = sum(warp_me2(img, tform))
                    c
                  end[2].H
       @test size(outp) == (3,3)
       @test eltype(outp) == eltype(h.H)
    end
+
    @testset "Colorant Types testing" begin
       outp = Zygote.gradient(img_plain, h) do img, tform
-                   c = L(warp_me2(img, tform))
+                   c = sum(warp_me2(img, tform))
                    c
                  end[2].H
       @test size(outp) == (3,3)
