df89dcf72fd6dde0545a12119054e798ea566847
SomTambe <tambesom@gmail.com>
2021-08-03 21:48:51 +0530

    Change tests layout

--- test/geometry/adjoints.jl
+++ test/geometry/adjoints.jl
@@ -5,20 +5,18 @@ etty = Interpolations.Extrapolation{Float64, 2, Interpolations.BSplineInterpolat
 
 @testset "Interpolations.BSplineInterpolation constructor gradient" begin
     zg = Zygote.gradient((x,y,z)->ty(x,y,z) |> sum, m, axs, BSpline(Linear()))
-    fd = grad(central_fdm(5,1), x->begin
-                                        ty(x, axs, BSpline(Linear())) |> sum
-                                    end, 
-                                    m)
+    fd = grad(central_fdm(5,1), 
+              x->ty(x, axs, BSpline(Linear())) |> sum, 
+              m)
     @test isapprox(zg[1], fd[1])
     @test zg[2:3] == (nothing, nothing)
 end
 
 @testset "Interpolations.BSplineInterpolation functor gradient" begin
     zg = Zygote.gradient((x,y,z,w)->Interpolations.BSplineInterpolation(x,y,z,w) |> sum, Float64, m, BSpline(Linear()), axs)
-    fd = grad(central_fdm(5,1), x->begin
-                                        Interpolations.BSplineInterpolation(Float64, x, BSpline(Linear()), axs) |> sum
-                                    end, 
-                                    m)
+    fd = grad(central_fdm(5,1), 
+              x->Interpolations.BSplineInterpolation(Float64, x, BSpline(Linear()), axs) |> sum, 
+              m)
     @test isapprox(zg[2], fd[1])
     @test findall(==(nothing), zg) == [1, 3, 4]
 end
@@ -26,20 +24,18 @@ end
 @testset "Interpolations.Extrapolation constructor gradient" begin
     itp = ty(m, axs, BSpline(Linear()))
     zg = Zygote.gradient((x,y)->etty(x,y) |> sum, itp, Flat())
-    fd = grad(central_fdm(5,1), x->begin
-                                        etty(itp, Flat()) |> sum
-                                    end, 
-                                    m)
+    fd = grad(central_fdm(5,1), 
+              x->etty(itp, Flat()) |> sum, 
+              m)
     @test isapprox(zg[1], fd[1])
     @test findall(==(nothing), zg) == [2]
 end
 
 @testset "Interpolations.copy_with_padding function gradient" begin
     zg = Zygote.gradient((x,y,z)->Interpolations.copy_with_padding(x,y,z) |> sum, Float64, m, BSpline(Linear()))
-    fd = grad(central_fdm(5,1), x->begin
-                                        Interpolations.copy_with_padding(Float64, x, BSpline(Linear())) |> sum
-                                    end, 
-                                    m)
+    fd = grad(central_fdm(5,1), 
+              x->Interpolations.copy_with_padding(Float64, x, BSpline(Linear())) |> sum, 
+              m)
     @test isapprox(zg[2], fd[1])
     @test findall(==(nothing), zg) == [1, 3]
 end
@@ -47,10 +43,9 @@ end
 @testset "Interpolations.FilledExtrapolation constructor gradient" begin
     itp = ty(m, axs, BSpline(Linear()))
     zg = Zygote.gradient((x,y)->Interpolations.FilledExtrapolation(x,y) |> sum, itp, 0.0)
-    fd = grad(central_fdm(5,1), x->begin
-                                        Interpolations.FilledExtrapolation(x, 0.0) |> sum
-                                    end, 
-                                    itp)
+    fd = grad(central_fdm(5,1), 
+              x->Interpolations.FilledExtrapolation(x, 0.0) |> sum, 
+              itp)
     @test isapprox(zg[1], fd[1])
     @test findall(==(nothing), zg) == [2]
 end
